aa8b6fbbb59484d0ea42d85e40696892e56a4991
fsniraj <fsniraj@fsnirajs-MacBook-Pro.local>
2023-01-31 22:40:02 +0530

    index.ts explain

---
+++ index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+// create a server
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!!!</h1>");
+});
+
+// listen to some port
+app.listen(8000, () => {
+  console.log("listening");
+});
