5645298de9c8ed73cf7c27e859692c5fa1c47a9c
fsniraj <fsniraj@fsnirajs-MacBook-Pro.local>
2023-01-31 23:29:41 +0530

    sending data

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,13 @@ import express from "express";
 // create a server
 const app = express();
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World, How Are You?</h1>");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/", (req, res) => {
+  // here we need data so that we can create new note/todo
+  console.log(req.body);
+  res.json({ message: "I am listening!" });
 });
 
 // listen to some port
